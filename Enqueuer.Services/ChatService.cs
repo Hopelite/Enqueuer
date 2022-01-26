2ce92fe66ba363eb9b3e1966566ed9a1459e754c
Vadim Kurdesov <KurdesoWadim@gmail.com>
2022-01-26 23:24:46 +0300

    feat: implement ChatService

---
+++ Enqueuer.Services/ChatService.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Enqueuer.Persistence.Models;
+using Enqueuer.Persistence.Repositories;
+using Enqueuer.Services.Interfaces;
+
+namespace Enqueuer.Services
+{
+    /// <inheritdoc/>
+    public class ChatService : IChatService
+    {
+        private readonly IRepository<Chat> chatRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChatService"/> class.
+        /// </summary>
+        /// <param name="chatRepository"><see cref="IRepository{T}"/> with <see cref="Chat"/> entities.</param>
+        public ChatService(IRepository<Chat> chatRepository)
+        {
+            this.chatRepository = chatRepository;
+        }
+
+        /// <inheritdoc/>
+        public async Task<Chat> GetNewOrExistingChat(long chatId)
+        {
+            var chat = this.chatRepository.GetAll()
+                .FirstOrDefault(chat => chat.ChatId == chatId);
+
+            if (chat is null)
+            {
+                chat = new Chat()
+                {
+                    ChatId = chatId,
+                };
+
+                await this.chatRepository.AddAsync(chat);
+            }
+
+            return chat;
+        }
+    }
+}
---
+++ Enqueuer.Services/Interfaces/IChatService.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Enqueuer.Persistence.Models;
+
+namespace Enqueuer.Services.Interfaces
+{
+    /// <summary>
+    /// Contains methods for <see cref="Chat"/>.
+    /// </summary>
+    public interface IChatService
+    {
+        /// <summary>
+        /// Gets already existing <see cref="Chat"/> or creates new one.
+        /// </summary>
+        /// <param name="chatId">Telegram chat ID to get by or create new with.</param>
+        /// <returns>Task with existing or newly created <see cref="Chat"/> in return.</returns>
+        public Task<Chat> GetNewOrExistingChat(long chatId);
+    }
+}
