499acc6c2f0c60829dd150b499b5cd844d05e8c5
Vadim Kurdesov <KurdesoWadim@gmail.com>
2022-01-26 23:43:11 +0300

    feat: initialize project tests and add chat service test

---
+++ Enqueuer.Tests/ServicesTests/ChatServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Enqueuer.Persistence;
+using Enqueuer.Persistence.Models;
+using Enqueuer.Persistence.Repositories;
+using Enqueuer.Services;
+using Enqueuer.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace Enqueuer.Tests.ServicesTests
+{
+    [TestFixture]
+    public class ChatServiceTests
+    {
+        private const string databaseName = "Test";
+        private EnqueuerContext context;
+        private IRepository<Chat> chatRepository;
+        private IChatService chatService;
+
+        public ChatServiceTests()
+        {
+            var dbContextOptions = new DbContextOptionsBuilder<EnqueuerContext>()
+                .UseInMemoryDatabase(databaseName).Options;
+            this.context = new EnqueuerContext(dbContextOptions);
+            this.chatRepository = new Repository<Chat>(context);
+            this.chatService = new ChatService(chatRepository);
+        }
+
+        [Test]
+        public async Task ChatServiceTests_GetNewOrExistingChat_CreatesAndReturnNewChat()
+        {
+            // Arrange
+            var expected = new Chat()
+            {
+                ChatId = 1,
+            };
+
+            // Act
+            var actual = await this.chatService.GetNewOrExistingChat(expected.ChatId);
+
+            // Asssert
+            Assert.AreEqual(expected.ChatId, actual.ChatId);
+        }
+    }
+}
