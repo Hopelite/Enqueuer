86c2528e1f3da89b97854be60e22b7b5900420f6
Vadim Kurdesov <KurdesoWadim@gmail.com>
2022-01-28 00:12:02 +0300

    feat: add bot project

---
+++ Enqueuer.Bot/IUpdateHandler.cs
@@ -0,0 +1,17 @@
+﻿using Telegram.Bot.Types;
+
+namespace Enqueuer.Bot
+{
+    /// <summary>
+    /// Handles incoming <see cref="Update"/>.
+    /// </summary>
+    public interface IUpdateHandler
+    {
+        /// <summary>
+        /// Handles incoming <see cref="Update"/>.
+        /// </summary>
+        /// <param name="update"><see cref="Update"/> to handle.</param>
+        /// <returns>Update handling task.</returns>
+        public Task HadnleUpdateAsync(Update update);
+    }
+}
---
+++ Enqueuer.Bot/UpdateHandler.cs
@@ -0,0 +1,26 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Enqueuer.Bot
+{
+    /// <inheritdoc/>
+    public class UpdateHandler : IUpdateHandler
+    {
+        private readonly ITelegramBotClient botClient;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UpdateHandler"/> class.
+        /// </summary>
+        /// <param name="telegramBotClient"><see cref="ITelegramBotClient"/> to use.</param>
+        public UpdateHandler(ITelegramBotClient telegramBotClient)
+        {
+            this.botClient = telegramBotClient;
+        }
+
+        /// <inheritdoc/>
+        public Task HadnleUpdateAsync(Update update)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
