2a0e9dd88f69898a29f7c238576206a94753c657
Vadim Kurdesov <KurdesoWadim@gmail.com>
2022-01-30 00:53:26 +0300

    feat: configure web project

---
+++ Enqueuer/Configuration/IBotConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace Enqueuer.Web.Configuration
+{
+    /// <summary>
+    /// Contains bot configuration.
+    /// </summary>
+    public interface IBotConfiguration
+    {
+        /// <summary>
+        /// Gets or sets bot access token.
+        /// </summary>
+        public string AccessToken { get; set; }
+
+        /// <summary>
+        /// Gets or sets application host.
+        /// </summary>
+        public string ApplicationHost { get; set;  }
+    }
+}
---
+++ Enqueuer/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Enqueuer.Persistence.Models;
+using Enqueuer.Persistence.Repositories;
+using Enqueuer.Services;
+using Enqueuer.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Enqueuer.Web.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository<Chat>, Repository<Chat>>();
+            services.AddScoped<IRepository<User>, Repository<User>>();
+            services.AddScoped<IRepository<Queue>, Repository<Queue>>();
+            services.AddTransient<IChatService, ChatService>();
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<IQueueService, QueueService>();
+
+            return services;
+        }
+    }
+}
