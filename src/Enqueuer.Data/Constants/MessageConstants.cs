6c6b75b65bb9a90a06e6d25d331940989fe85a36
hopelite <KurdesoWadim@gmail.com>
2023-01-25 19:04:11 +0100

    feat: update commands

--- src/Enqueuer.Data/Constants/MessageConstants.cs
+++ src/Enqueuer.Data/Constants/MessageConstants.cs
@@ -5,36 +5,36 @@
         /// <summary>
         /// Gets create queue command.
         /// </summary>
-        public static string CreateQueueCommand => "/createqueue";
+        public static string CreateQueueCommand => "/creerliste";
 
         /// <summary>
         /// Gets dequeue command.
         /// </summary>
-        public static string DequeueCommand => "/dequeue";
+        public static string DequeueCommand => "/supmbreliste";
 
         /// <summary>
         /// Gets enqueue command.
         /// </summary>
-        public static string EnqueueCommand => "/enqueue";
+        public static string EnqueueCommand => "/ajtmbreliste";
 
         /// <summary>
         /// Gets help command.
         /// </summary>
-        public static string HelpCommand => "/help";
+        public static string HelpCommand => "/aide";
 
         /// <summary>
         /// Gets queue command.
         /// </summary>
-        public static string QueueCommand => "/queue";
+        public static string QueueCommand => "/liste";
 
         /// <summary>
         /// Gets remove queue command.
         /// </summary>
-        public static string RemoveQueueCommand => "/removequeue";
+        public static string RemoveQueueCommand => "/supliste";
 
         /// <summary>
         /// Gets start command.
         /// </summary>
-        public static string StartCommand => "/start";
+        public static string StartCommand => "/commencer";
     }
 }
