45d7d81069043366379da11e7df67ed2a4efcf9a
Vadzim Kurdzesau <VKurdzesau@slb.com>
2023-04-24 22:28:31 +0200

    refactor: make command context properties readonly

--- src/Enqueuer.Telegram.Sessions/Types/CommandContext.cs
+++ src/Enqueuer.Telegram.Sessions/Types/CommandContext.cs
@@ -11,17 +11,25 @@ public class CommandContext
     /// <summary>
     /// Command text without parameters.
     /// </summary>
-    public string Command { get; init; }
+    public string Command { get; }
 
     /// <summary>
     /// Command parameters that were either specified after the command text or added to the context later. 
     /// </summary>
-    public IDictionary<string, CommandParameter> Parameters { get; init; } = new Dictionary<string, CommandParameter>();
+    public IDictionary<string, CommandParameter> Parameters { get; } = new Dictionary<string, CommandParameter>();
 
     public CommandContext(string command)
     {
         Command = string.IsNullOrWhiteSpace(command)
             ? throw new ArgumentNullException(nameof(command), "Command can't be null, empty or a whitespace.")
             : command;
+    }
+
+    internal CommandContext(string command, IDictionary<string, CommandParameter> parameters)
+    {
+        Command = string.IsNullOrWhiteSpace(command)
+            ? throw new ArgumentNullException(nameof(command), "Command can't be null, empty or a whitespace.")
+            : command;
+        Parameters = parameters ?? throw new ArgumentNullException(nameof(parameters));
     }
 }
